a75f34b7e131fe2910b9e56472d0c3497513ffa5
Bartłomiej Żebrowski <89999775+kosiarnikow@users.noreply.github.com>
2023-11-08 13:45:43 +0100

    dodanie zadanie1 - tydzień1

---
+++ tydzien1/zadanie1/kalkulator.js
@@ -0,0 +1,27 @@
+function calculate() {
+    const liczba1 = parseFloat(document.getElementById("liczba1").value);
+    const liczba2 = parseFloat(document.getElementById("liczba2").value);
+    const operacja = document.getElementById("operacja").value;
+    let wynik;
+
+    switch (operacja) {
+        case "dodaj":
+            wynik = liczba1 + liczba2;
+            break;
+        case "odejmij":
+            wynik = liczba1 - liczba2;
+            break;
+        case "pomnoz":
+            wynik = liczba1 * liczba2;
+            break;
+        case "podziel":
+            if (liczba2 === 0) {
+                wynik = "nie wolno dzielić przez zero XD";
+            } else {
+                wynik = liczba1 / liczba2;
+            }
+            break;
+    }
+
+    document.getElementById("wynik").textContent = wynik;
+}
