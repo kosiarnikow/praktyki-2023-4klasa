975d6dbc7630626147dd3278ede645e507a93a8d
Bartłomiej Żebrowski <89999775+kosiarnikow@users.noreply.github.com>
2023-11-19 21:38:54 +0100

    poprawione zadanie 7

--- tydzień3/zadania_na_11_20/zadanie7/script.js
+++ tydzień3/zadania_na_11_20/zadanie7/script.js
@@ -1,18 +1,17 @@
 const books = [
-    {title: 'Total loss 100', pages: 600, genre: 'fantasy', rating: 7}, 
-    {title: 'Total enlightenment', pages: 250, genre: 'romance', rating: 4}, 
-    {title: 'Big loss', pages: 400, genre: 'fantasy', rating: 2}, 
-    {title: 'Tenth Joy', pages: 32, genre: 'action', rating: 9}, 
-    {title: 'Quickfix number 4', pages: 15, genre: 'fantasy', rating: 8}, // to jest, wiec trzeba zapisac
-    {title: 'World Ender 3', pages: 199, genre: 'fantasy', rating: 1},
-    {title: 'Paranormal', pages: 200, genre: 'thriller', rating: 5}, 
+  { title: 'Total loss 100', pages: 600, genre: 'fantasy', rating: 7 },
+  { title: 'Total enlightenment', pages: 250, genre: 'romance', rating: 4 },
+  { title: 'Big loss', pages: 400, genre: 'fantasy', rating: 2 },
+  { title: 'Tenth Joy', pages: 32, genre: 'action', rating: 9 },
+  { title: 'Quickfix number 4', pages: 15, genre: 'fantasy', rating: 8 },
+  { title: 'World Ender 3', pages: 199, genre: 'fantasy', rating: 1 },
+  { title: 'Paranormal', pages: 200, genre: 'thriller', rating: 5 },
 ];
 
-const countPositiveRatings = (books) => {
-  const positiveRatings = books.filter(
-    (book) => book.pages % 2 !== 0 && book.title.includes(/\d/) && book.rating > 5
-  );
-  console.log("ilosc ksiazek spelniajace wymagania:", positiveRatings.length);
-};
+const countPositiveRatings = (books) =>
+  books
+    .filter((book) => book.pages % 2 !== 0 && /\d/.test(book.title))
+    .filter((book) => book.rating > 5)
+    .length;
 
-countPositiveRatings(books);
+console.log("ilosc ksiazek spełniające podane wymagania: ", countPositiveRatings(books));
